137e39231d4c1fd5caf93ffb0f49b937eab5fd7a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-05-30 21:18:36 +1000

    prtikl

--- Lab_3ver2/program.js
+++ Lab_3ver2/program.js
@@ -52,6 +52,8 @@ let correspond = {
     "Экипаж (чел.)":  ["crew_min", "crew_max"],
 } 
 
+// ФИЛЬТР ДАННЫХ ----------------------------------------------------
+
 let filter_data = (dataForm) => {          
     let dictFilter = {};     
     // перебираем все элементы формы с фильтрами          
@@ -94,5 +96,44 @@ let filter_table = (dataForm) =>{
 let cancel_filter_table = (dataForm) => {
     dataForm.reset();
     local_data = technique;
+    rebuild_table(local_data);
+}
+
+// СОРТИРОВКА ----------------------------------------------------
+
+let sort_data = (data, key, direction) => {
+    let sorted = data.sort((a, b) => {
+        if (direction === 'asc') {
+            return a[key] - b[key];
+        } else if (direction === 'desc') {
+            return b[key] - a[key];
+        }
+    });
+
+    return sorted;
+}
+
+let sort_table = (dataForm) => {
+
+    I_key = dataForm.I_name.value
+    I_direction = dataForm.I_reverse.checked ? 'desc' : 'asc';
+    alert(I_key)
+    alert(I_direction)
+    if (I_key != "НЕТ") {
+        local_data = sort_data(local_data, I_key, I_direction);
+    }
+
+    II_key = dataForm.II_name.value
+    II_direction = dataForm.II_reverse.checked ? 'desc' : 'asc';
+    if (I_key != "НЕТ") {
+        local_data = sort_data(local_data, II_key, II_direction);
+    }
+
+    III_key = dataForm.III_name.value
+    III_direction = dataForm.III_reverse.checked ? 'desc' : 'asc';
+    if (I_key != "НЕТ") {
+        local_data = sort_data(local_data, III_key, III_direction);
+    }
+
     rebuild_table(local_data);
 }
